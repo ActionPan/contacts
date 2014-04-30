9de0c3b8cd289a37468ab0a6ddbaeb83757743f7
panruigang <690019756@qq.com>
2014-04-30 10:53:19 +0800

    frist commit

---
+++ platforms/android/bin/MyContacts-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\contacts\platforms\android\bin\MyContacts-debug-unaligned.apk : \
+D:\contacts\platforms\android\bin\MyContacts.ap_ \
+D:\contacts\platforms\android\bin\classes.dex \
+D:\contacts\platforms\android\CordovaLib\bin\classes.jar \
